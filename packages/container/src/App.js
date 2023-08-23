0f84af35a133bcd56e9ebe1598cc2b88cfabfcb8
hridayanp <hridayanphukan123@gmail.com>
2023-08-23 16:34:25 +0530

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+// import { useHistory } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
