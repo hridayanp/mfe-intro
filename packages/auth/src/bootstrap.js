395371257f276dcc8dede23b390094a1a8751e3b
hridayanp <hridayanphukan123@gmail.com>
2023-09-02 20:35:03 +0530

    added auth app and its webpack config

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
 
+// Mount function to start up the app
 const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   const history =
     defaultHistory ||
@@ -10,7 +11,9 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
       initialEntries: [initialPath],
     });
 
-  if (onNavigate) history.listen(onNavigate);
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
 
   ReactDOM.render(<App history={history} />, el);
 
@@ -28,13 +31,13 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
 // If we are in development and in isolation,
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
-    mount(devRoot, {
-      defaultHistory: createBrowserHistory(),
-    });
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense } from 'react';
 import Header from './components/Header';
 // import { useHistory } from 'react-router-dom';
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Progress from './components/Progress';
+
+const LazyMarketingApp = lazy(() => import('./components/MarketingApp'));
+
+const LazyAuthenticationApp = lazy(() =>
+  import('./components/AuthenticationApp')
+);
 
 export default () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <hr />
-        <MarketingApp />
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={LazyAuthenticationApp} />
+            <Route path="/" component={LazyMarketingApp} />
+          </Switch>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
